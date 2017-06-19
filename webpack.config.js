228353e1fdb277c4781db23a5f6e2a0676cd3415
omar jakmira <poweromar19942@gmail.com>
2017-06-19 10:46:17 -0800

    auto inject script in index.html using htmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 var webpack = require('webpack');
 var path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+const VENDOR_LIBS = [
+    "faker","lodash","react","react-dom",
+    "react-input-range","react-redux","react-router",
+    "redux", "redux-form", "redux-thunk",
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+      bundle:'./src/index.js',
+      vendor:VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
     module:{
         rules:[
@@ -18,5 +27,13 @@ module.exports = {
                 test:/\.css$/
             }
         ]
-    }
+    },
+    plugins:[
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'vendor'
+        }),
+        new htmlWebpackPlugin({
+          template:'src/index.html'
+        })
+    ]
 };
